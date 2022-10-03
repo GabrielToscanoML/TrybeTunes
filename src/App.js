0e0f3e7b35f0056d495e0dff111e155fcbd05784
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-10-03 17:04:12 -0300

    requisito 1 concluído

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFoud';
 
 class App extends React.Component {
   render() {
-    return (<p>Trybe Tunes</p>);
+    return (
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
