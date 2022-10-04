4f6aa24224d43f5e941ba82347597ee4aaaa37ff
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-10-04 19:10:02 -0300

    requisito 6 feito uns 70%

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 
 class Search extends React.Component {
   // praticamente copiei o formulario do Login e adaptei
   state = {
     artistInput: '',
     isButtonDisabled: true,
+    loading: false,
+    isOK: false,
+    infoList: [],
+    inputCopy: '',
+  };
+
+  requestAPI = async () => {
+    const { artistInput } = this.state;
+    this.setState({ loading: true });
+    const response = await searchAlbumsAPI(artistInput);
+    this.setState({ infoList: response });
+    this.setState({ loading: false });
+    this.setState({ isOK: true });
+    this.setState({ inputCopy: artistInput }); // limpando o input
+    this.setState({ artistInput: '' }); // limpando o input
+    console.log(response);
   };
 
   handleInputOnChange = (event) => {
@@ -24,35 +42,66 @@ class Search extends React.Component {
   };
 
   render() {
-    const { isButtonDisabled, artistInput } = this.state;
+    const { isButtonDisabled, artistInput,
+      loading, isOK, infoList, inputCopy } = this.state;
+
     return (
       <div data-testid="page-search">
         <Header />
-        <form>
-          <label htmlFor="search-artist">
-            <input
-              // name="userName"
-              className="search-artist"
-              data-testid="search-artist-input"
-              placeholder="Nome do Artista:"
-              type="text"
-              value={ artistInput }
-              onChange={ this.handleInputOnChange }
-            />
-          </label>
+        {
+          loading
+            ? <Loading />
+            : (
+              <form>
+                <label htmlFor="search-artist">
+                  <input
+                    // name="userName"
+                    className="search-artist"
+                    data-testid="search-artist-input"
+                    placeholder="Nome do Artista:"
+                    type="text"
+                    value={ artistInput }
+                    onChange={ this.handleInputOnChange }
+                  />
+                </label>
+                <button
+                  data-testid="search-artist-button"
+                  className="search-artist-button"
+                  type="button"
+                  disabled={ isButtonDisabled }
+                  onClick={ this.requestAPI }
+                >
+                  Pesquisar
+                </button>
+              </form>
+            )
+        }
 
-          <button
-            data-testid="search-artist-button"
-            className="search-artist-button"
-            type="button"
-            disabled={ isButtonDisabled }
-          >
-            Pesquisar
-          </button>
-        </form>
+        { isOK && (
+          <div>
+            <p>
+              Resultado de álbuns de:
+              {' '}
+              {inputCopy}
+            </p>
+          </div>)}
+
+        { infoList.length === 0 && (
+          <div>
+            <p> Nenhum álbum foi encontrado </p>
+          </div>)}
+
+        {
+          infoList.map((album) => (
+            <div key="">
+              <p>{ album.artistName }</p>
+              <p>{ album.collectionName }</p>
+              <img src={ album.artworkUrl100 } alt={ album.artistName } />
+            </div>
+          ))
+        }
       </div>
     );
   }
 }
-
 export default Search;
