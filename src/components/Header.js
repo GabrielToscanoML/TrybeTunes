cf27dc79f99d78d2edfd46c95cf0c4fe2221524b
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-10-04 16:23:17 -0300

    requisito 3 e 4 concluidos

---
+++ src/components/Header.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import Loading from './Loading';
+import './header.css';
+
+class Header extends React.Component {
+  state = {
+    loading: false,
+    userName: '',
+  };
+
+  componentDidMount() {
+    this.getUserInfo();
+  }
+
+  getUserInfo = async () => {
+    this.setState({ loading: true });
+    const name = await getUser();
+    this.setState({ userName: name.name });
+    this.setState({ loading: false });
+  };
+
+  render() {
+    const { loading, userName } = this.state;
+    return (
+      <div data-testid="header-component">
+        {
+          loading
+            ? <Loading />
+            : (
+              <div>
+                <h1 data-testid="header-user-name">{ userName }</h1>
+                <nav id="nav-links">
+                  <Link to="/search" data-testid="link-to-search"> Search </Link>
+                  <Link
+                    to="/favorites"
+                    data-testid="link-to-favorites"
+                    id="link-favorites"
+                  >
+                    Favorites
+                  </Link>
+                  <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+                </nav>
+              </div>
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default Header;
